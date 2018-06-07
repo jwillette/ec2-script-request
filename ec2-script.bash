c68136e4780d74709afe0826d18f31ad175d1b90
jwillette <jwillette@mymail.tstc.edu>
2018-06-07 01:25:22 -0500

    tested script

--- ec2-script.bash
+++ ec2-script.bash
@@ -5,7 +5,7 @@ aws configure
 
 #ask user for their key pair name
 read -p 'Key Pair Name: ' keyname
-read -p 'Key Pair PEM Path: ' keypath
+read -p 'Key Pair PEM Path (leaving off the final slash): ' keypath
 read -p 'Security Group: ' securitygroup
 
 #launch an Ubuntu 16.04 EC2 instance
@@ -15,28 +15,4 @@ aws ec2 run-instances --image-id ami-a4dc46db --count 1 --instance-type t2.micro
 read -p 'Public DNS of new instance: ' ec2dns
 
 #ssh into the new EC2 instance to execute remote commands
-ssh -i $keypath/$keyname.pem ubuntu@$ec2dns
-
-#install operating system updates
-sudo apt-get update && sudo apt-get upgrade
-
-#install apache
-sudo apt-get install apache2 -y
-
-#ensure apache is on
-sudo systemctl start apache2
-
-#create a group and add the ubuntu user to it
-groupadd www
-usermod -a -G www ubuntu
-chown -R root:www /var/www
-
-#change the directory permissions of /var/www and its subdirectories to add group write permissions and set the group ID on subdirectories created in the future
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} +
-
-#recursively change the permissions for files in the /var/www directory and its subdirectories to add group write permissions
-find /var/www -type f -exec chmod 0664 {} +
-
-# set Apache's index.html file to display the text "Hello, World!"
-echo "Hello, World!" > /var/www/html/index.html
+ssh -i $keypath/$keyname.pem ubuntu@$ec2dns 'sudo apt-get update -y && sudo apt-get upgrade -y; sudo apt-get install apache2 -y; sudo systemctl start apache2; sudo groupadd www; sudo usermod -a -G www ubuntu; sudo chown -R root:www /var/www; sudo chmod 2775 /var/www; sudo find /var/www -type d -exec chmod 2775 {} +; sudo find /var/www -type f -exec chmod 0664 {} +; echo "Hello, World!" > /var/www/html/index.html'
